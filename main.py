51d744aecbfe2cd6db0bd3d9d012173a76dbb216
flatcebo <organicsic@naver.com>
2023-08-07 17:31:31 +0900

    scond

--- main.py
+++ main.py
@@ -1,114 +1,152 @@
 # pip install requests && beautifulsoup
 import requests
+import time
+import asyncio
+import re
+import pymysql
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
-url = 'http://jowhang.dinak.co.kr/%EC%A0%90%EC%A3%BC%EC%84%A0%EC%9E%A5%EC%A1%B0%ED%99%A9/list?sYear=2020&tmp=1'
-detailUrl = 'http://jowhang.dinak.co.kr/%EC%A0%90%EC%A3%BC%EC%84%A0%EC%9E%A5%EC%A1%B0%ED%99%A9/list?sYear=2020&tmp=1'
 
-response = requests.get(url)
-detailResponse = requests.get(detailUrl)
+connection = pymysql.connect(
+    host='192.168.0.37',  
+    port=3306,  
+    user='localDB',
+    password='flatcebo',
+    db='crawling',  
+    charset='utf8'
+)
 
-html = response.text
-soup = BeautifulSoup(html, 'html.parser')
-ul = soup.select_one('.maincontents')
-urls = []
-
-# 제목
-# titles = ul.select(".contents-wrapper tbody td.subject a")
-# for title in titles:
-#     print(title.get_text())
-# # 업체명
-# userNames = ul.select(".contents-wrapper tbody td.writer.member span")
-# for userName in userNames:
-#     print(userName.get_text())
-# 주소
-subjects = ul.select(".contents-wrapper table tbody td.subject a")
-for subject in subjects:
-    print(subject.attrs['href'])
-    urls.append(subject.attrs['href'])
-# 게시일
-# uploadAts = ul.select(".contents-wrapper tbody td.date")
-# for uploadAt in uploadAts:
-#     print(uploadAt.get_text())
-# # 유저 아이디
-# userIds = ul.select(".contents-wrapper tbody td.writer.member span")
-# for userId in userIds:
-#     print(userId.attrs["onclick"].split(', ')[3].strip().strip("'"))
-# # 지역
-# regions = ul.select(".contents-wrapper tbody td.region span")
-# for region in regions:
-#     print(region.get_text())
-# # 글번호
-# listNumbs = ul.select(".contents-wrapper tbody td.no")
-# for listNumb in listNumbs:
-#     print(listNumb.get_text())
-# # 타입
-# types = ul.select(".contents-wrapper tbody td.type .typeicon")
-# for type in types:
-#     print(type.get_text())
-
-    
-
-visited_urls = set()
-# url = "http://jowhang.dinak.co.kr/%EC%A0%90%EC%A3%BC%EC%84%A0%EC%9E%A5%EC%A1%B0%ED%99%A9"
-# url = "http://jowhang.dinak.co.kr/%EC%A0%90%EC%A3%BC%EC%84%A0%EC%9E%A5%EC%A1%B0%ED%99%A9"
-
-def crawl_page(url,urls):
-    global visited_urls
-
-    if url in visited_urls:
-        return
-
-    visited_urls.add(url)
-
-    print(f"Crawling: {url}")
-
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            html_content = response.text
-            soup = BeautifulSoup(html_content, 'html.parser')
-
-            # 원하는 데이터 추출 또는 처리
-            # 예를 들어, 해당 페이지의 제목 출력
-            # title = soup.title.string
-            # print(f"Page Title: {title}")
-            # print(soup.select("td.subject a",href=True))
-
-            # 페이지의 모든 링크를 가져와서 재귀적으로 크롤링
-            # count = 0
-            # links = soup.select("td.subject a", href=True)
-            # for link in links:
-            #     # count += 1
-            #     next_url = urljoin(url, link['href'])
-            #     urls.append(next_url)
-            #     crawl_page(next_url, urls)
-            # print("count =", count)
-
-                
-
-    except Exception as e:
-        print(f"Error crawling {url}: {e}" )
+cur = connection.cursor()
 
 
 
-        # detailPhoneNumber:
-#         (detailDesc as any).text().trim().replace(/\D/g, "|") ?? "",
-#  const regex: any =
-#       /(?=.)(?:02|0[13-9]{1}[0-9]{1})[^0-9]*[0-9]{3,4}[^0-9]*[0-9]{4}/g;
-#     let result: any = regex.exec(detailItems.detailDesc);
-#     let results: any = [];
-#     while (result != null) {
-#       results.push(result[0]);
-#       console.log("phone =>", result[0]);
-#       result = regex.exec(detailItems.detailDesc);
-#     }
+def crawl_list(url) :
+
+    response = requests.get(url)
+
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+    ul = soup.select_one('.maincontents')
+    # urls = []
+
+    # 제목
+    arr = []
+    titles = ul.select(".contents-wrapper tbody td.subject a")
+    for title in titles:
+        title.get_text()
+    # 업체명
+    userNames = ul.select(".contents-wrapper tbody td.writer.member span")
+    for userName in userNames:
+        userName.get_text()
+    # 주소
+    subjects = ul.select(".contents-wrapper table tbody td.subject a")
+    for subject in subjects:
+        subject.attrs['href']
+        # urls.append(subject.attrs['href'])
+    # 게시일
+    uploadAts = ul.select(".contents-wrapper tbody td.date")
+    for uploadAt in uploadAts:
+        uploadAt.get_text()
+    # 유저 아이디
+    userIds = ul.select(".contents-wrapper tbody td.writer.member span")
+    for userId in userIds:
+        userId.attrs["onclick"].split(', ')[3].strip().strip("'")
+    # 지역
+    regions = ul.select(".contents-wrapper tbody td.region span")
+    for region in regions:
+        region.get_text()
+    # 글번호
+    listNumbs = ul.select(".contents-wrapper tbody td.no")
+    for listNumb in listNumbs:
+        listNumb.get_text()
+    # 타입
+    types = ul.select(".contents-wrapper tbody td.type .typeicon")
+    for type in types:
+        type.get_text()
+
+    for i in range(len(subjects)):
+        arr.append({
+            "title":titles[i].get_text(),
+            "userName": userNames[i].get_text(),
+            "subject": subjects[i].attrs['href'],
+            "uploadAt": uploadAts[i].get_text(),
+            "userId":userIds[i].attrs["onclick"].split(', ')[3].strip().strip("'"),
+            "region": regions[i].get_text(),
+            "listNumb":listNumbs[i].get_text(),
+            "type": types[i].get_text()
+        })
+
+    return arr
+
+def crawl_next(url) :
+    # global visited_urls
+    # visited_urls = set()
+
+    # if url in visited_urls:
+    #     return
+
+    response = requests.get(url)
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+    ul = soup.select_one('.contents')
+    count = 0
+    links = soup.select("td.subject a", href=True)
+    for link in links:
+        if count == 1:
+            break
+        next_url = urljoin(print("joinUrl ===>",url),print("link =>",link['href']))
+        count += 1
+
+
+        # 상세글
+        detailDescs = ul
+        for detailDesc in detailDescs : 
+            detailDesc
+
+        # 상세 이미지
+        detailImgs = ul.select('img')
+        for detailImg in detailImgs :
+            detailImg.attrs['src']
+        
+        # 상세 번호
+        detailNumbs = ul
+        for detailNumb in detailNumbs :
+            regexNumb = detailNumb.get_text()
+            pattern = re.compile(r'(?=.)(?:010)[^0-9]*[0-9]{3,4}[^0-9]*[0-9]{4}')
+            results = pattern.findall(regexNumb)
+
+            for result in results:
+                print(result)
+try:
+    with connection.cursor() as cursor:
+        sql = "INSERT INTO users (username) VALUES (%s)"
+        data = ('dd')
+        cursor.execute(sql, data)
+    connection.commit()
+finally:
+    connection.close()
 
 
 
 if __name__ == "__main__":
-    # start_url = urls  # 크롤링을 시작할 URL 설정
-    # crawl_page(start_url)
-    for url in urls:
-        crawl_page(url, urls)
+    url = "http://jowhang.dinak.co.kr/%EC%A0%90%EC%A3%BC%EC%84%A0%EC%9E%A5%EC%A1%B0%ED%99%A9/list?sYear=2020&tmp=1"
+    page = 1
+    totalPages = 2
+    delayInMilliseconds = 3
+    arr = []
+    while True:
+        if page >= totalPages:
+            print("break")
+            break
+       
+        print("페이지 번호 =>", page)
+        pages = f"&page={page}"
+        arr += crawl_list(url + pages)
+
+        page += 1
+
+        time.sleep(delayInMilliseconds)
+
+    for item in arr:
+        crawl_next(item["subject"])
